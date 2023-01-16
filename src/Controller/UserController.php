3f3f36917707c7b76a4fb2019a032ddb8125aaa9
Fizzixx4 <gregory2koch@yahoo.fr>
2023-01-16 14:56:29 +0100

    création util v1

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserCreationFormType;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,6 +17,21 @@ class UserController extends AbstractController
     {
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
+        ]);
+    }
+
+    #[Route('/create_user', name: 'createUser')]
+    public function createUser(UserRepository $userRepo, Request $request): Response
+    {   
+        $user = new User();
+        $form = $this->createForm(UserCreationFormType::class, $user);
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            $user->setRoles(["ROLE_USER"]);
+            $userRepo->save($user, true);
+        }
+        return $this->render('user/create_user.html.twig', [
+            'form_user' => $form->createView(),
         ]);
     }
 }
