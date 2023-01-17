cde0a70a30071ecb6b7eeef7783b454ca03a668a
Fizzixx4 <gregory2koch@yahoo.fr>
2023-01-17 20:53:21 +0100

    form update user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -27,18 +27,20 @@ class UserController extends AbstractController
         ]);
     }
 
-    // Page de création de User
-    #[Route('/create_user', name: 'createUser')]
-    public function createUser(UserRepository $userRepo, Request $request): Response
-    {   
+    //Affichage Formulaire pour l'entité User
+    private function formUser(UserRepository $userRepo, Request $request, User $user):Response
+    {
         $message = '';
-        $user = new User();
         $form = $this->createForm(UserCreationFormType::class, $user);
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid()){
             $user->setRoles(["ROLE_USER"]);
             $userRepo->save($user, true);
-            $message = 'L\'utilisateur a bien été créé';
+            if($request->get('id')){
+                $message = 'L\'utilisateur a bien été modifié';
+            }else{
+                $message = 'L\'utilisateur a bien été créé';
+            }
         }
         elseif($form->isSubmitted()){
             $message = 'Les informations ne sont pas valides, ou ce compte existe déjà';
@@ -47,5 +49,20 @@ class UserController extends AbstractController
             'form_user' => $form->createView(),
             'message' => $message
         ]);
+    }
+
+    // Page de création du User
+    #[Route('/create_user', name: 'createUser')]
+    public function createUser(UserRepository $userRepo, Request $request)
+    {   
+        $user = new User();
+        return $this->formUser($userRepo, $request, $user);
+    }
+
+    //Page de modification du User
+    #[Route('/update_user/{id}', name: 'updateUser')]
+    public function updateUser(User $user, UserRepository $userRepo, Request $request)
+    {   
+        return $this->formUser($userRepo, $request, $user);
     }
 }
