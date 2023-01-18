682842c8a369a31131704b56c1bc10ece4974713
Lacerda <soulaynicolas@gmail.com>
2023-01-19 00:14:42 +0100

    on a les marqueurs sur la map

--- assets/map.js
+++ assets/map.js
@@ -6,6 +6,26 @@ const longitude = document.getElementById('longitude').value;
 
 const zoomLevel = document.getElementById('zoomLevel').value;
 
+const greenIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
+const redIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
+
+//console.log(coordinatesProduits)
 
 let map = L.map('map').setView([latitude, longitude], zoomLevel); 
 //zoom level: 
@@ -19,9 +39,18 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+L.marker([latitude, longitude], {icon: redIcon}).addTo(map)
 
-function addProductMarker() {
+addProductMarker(coordinatesProduits);
 
-    let marker = L.marker([lat, long]).addTo(map);
-    
+function addProductMarker(coordinates) {
+    {
+        for (let i=0; i < coordinates.length; i++){
+            coordinates[i] = coordinates[i].split(",");
+            
+            let lat= coordinates[i][0];
+            let long= coordinates[i][1];
+            L.marker([lat, long]).addTo(map).bindPopup("<b>Ici il y aura de belles choses un jour</b>");
+        }
+    };
 }
