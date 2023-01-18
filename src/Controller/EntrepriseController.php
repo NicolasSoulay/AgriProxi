75d0c01bfa449d205296e0eaea83efd13ea831d5
JustineRgs <108766192+JustineRgs@users.noreply.github.com>
2023-01-18 15:21:54 +0100

    page/entreprise

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -18,31 +18,48 @@ use Symfony\Component\Routing\Annotation\Route;
 class EntrepriseController extends AbstractController
 {
     #[Route('/entreprise', name: 'app_entreprise')]
-    public function index(): Response
+    public function index(EntrepriseRepository $entrepriseRepo, Request $request): Response
     {
+        $entreprises = $entrepriseRepo->findAll();
         return $this->render('entreprise/index.html.twig', [
             'controller_name' => 'EntrepriseController',
+            'entreprises' => $entreprises
         ]);
     }
 
+    #[Route('/entreprise/{id}', name: 'viewEntreprise')]
+    public function show(string $id, EntrepriseRepository $entrepriseRepo, Request $request): Response
+    {
+        $entreprise = $entrepriseRepo->find($id);
+        $adresses = $entreprise->getAdresses();
+        $produits = $entreprise->getProduits();
+        return $this->render('entreprise/show.html.twig', [
+            'controller_name' => 'EntrepriseController',
+            'entreprise' => $entreprise,
+            'adresses' => $adresses,
+            'produits' => $produits
+        ]);
+    }
+
+
     //Affichage Formulaire pour l'entité Entreprise
-    private function formEntreprise(Entreprise $entreprise, EntrepriseRepository $entrepriseRepo, UserRepository $userRepo, Request $request){
+    private function formEntreprise(Entreprise $entreprise, EntrepriseRepository $entrepriseRepo, UserRepository $userRepo, Request $request)
+    {
         $message = '';
         $form = $this->createForm(EntrepriseCreationFormType::class, $entreprise);
         $form->handleRequest($request);
         $userId = $this->getUser()->getId();
         $user = $userRepo->find($userId);
-        if($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $entrepriseRepo->save($entreprise, true);
             $user->setEntreprise($entreprise);
             $userRepo->save($user, true);
-            if($request->get('id')){
+            if ($request->get('id')) {
                 $message = 'L\'entreprise a bien été modifiée';
-            }else{
+            } else {
                 $message = 'L\'entreprise a bien été créée';
             }
-        }
-        elseif($form->isSubmitted()){
+        } elseif ($form->isSubmitted()) {
             $message = 'Les informations ne sont pas valides, ou cette entreprise existe déjà';
         }
 
@@ -55,7 +72,7 @@ class EntrepriseController extends AbstractController
     //Page de création d'entreprise
     #[Route('/create_entreprise', name: 'createEntreprise')]
     public function createEntreprise(EntrepriseRepository $entrepriseRepo, UserRepository $userRepo, Request $request): Response
-    {   
+    {
         $entreprise = new Entreprise();
         return $this->formEntreprise($entreprise, $entrepriseRepo, $userRepo, $request);
     }
@@ -63,27 +80,27 @@ class EntrepriseController extends AbstractController
     //Page de modification de l'entreprise
     #[Route('/update_entreprise/{id}', name: 'updateEntreprise')]
     public function updateEntreprise(Entreprise $entreprise, EntrepriseRepository $entrepriseRepo, UserRepository $userRepo, Request $request): Response
-    {   
+    {
         return $this->formEntreprise($entreprise, $entrepriseRepo, $userRepo, $request);
     }
 
 
     //Affichage Formulaire pour l'entité Adresse
-    private function formAdresse(Adresse $adresse, AdresseRepository $adresseRepo, Request $request){
+    private function formAdresse(Adresse $adresse, AdresseRepository $adresseRepo, Request $request)
+    {
         $message = '';
         $form = $this->createForm(AdresseCreationFormType::class, $adresse);
         $form->handleRequest($request);
         $entreprise = $this->getUser()->getEntreprise();
-        if($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $adresse->setEntreprise($entreprise);
             $adresseRepo->save($adresse, true);
-            if($request->get('id')){
+            if ($request->get('id')) {
                 $message = 'L\'adresse a bien été modifiée';
-            }else{
+            } else {
                 $message = 'L\'adresse a bien été créée';
             }
-        }
-        elseif($form->isSubmitted()){
+        } elseif ($form->isSubmitted()) {
             $message = 'Les informations ne sont pas valides';
         }
         return $this->render('entreprise/create_adresse.html.twig', [
@@ -108,7 +125,7 @@ class EntrepriseController extends AbstractController
     }
 
     #[Route('/entreprise/delete_adresse/{id}', name: 'deleteAdresse')]
-    public function deleteAdresse(Adresse $adresse, AdresseRepository $adresseRepo):Response
+    public function deleteAdresse(Adresse $adresse, AdresseRepository $adresseRepo): Response
     {
         $adresseRepo->remove($adresse, true);
         return $this->redirectToRoute('app_user');
