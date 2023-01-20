354da1b51fdcbda01b3be15124546d8bf724c878
Fizzixx4 <gregory2koch@yahoo.fr>
2023-01-20 17:21:38 +0100

    .

--- assets/adresse.js
+++ assets/adresse.js
@@ -1,6 +1,7 @@
-const inputVille = document.getElementById('adresse_creation_form_ville');
-const value = inputVille.value;
+// const inputVille = document.getElementById('adresse_creation_form_ville');
+// const value = inputVille.value;
 
-inputVille.addEventListener('input', (event) => {
-    value.textContent = event.target.value;
-})
+// inputVille.addEventListener('input', (event) => {
+//     let truc = event.target.value;
+//     fetch().then(response => response.json())
+// })
