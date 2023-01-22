cd50544f12a5662769aa1c36695ae73893432e87
JustineRgs <108766192+JustineRgs@users.noreply.github.com>
2023-01-22 10:07:00 +0100

    dev

--- src/Controller/DevisController.php
+++ src/Controller/DevisController.php
@@ -11,6 +11,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityRepository;
+use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
 
 class DevisController extends AbstractController
 {
@@ -41,7 +43,8 @@ class DevisController extends AbstractController
             return $this->render('devis/index.html.twig', [
                 "produits" => $grouped_produits,
                 "devis" => $devis,
-                "types" => $type
+                "types" => $type,
+                "ligneDevis" => $ligneDevis,
             ]);
         }
     }
@@ -50,6 +53,46 @@ class DevisController extends AbstractController
     #[Route('/devis/add/{id}', name: 'add_devis')]
     public function addProductDevis($id, ProduitRepository $produitRepo, DevisRepository $devisRepo, LigneDevisRepository $ligneRepo)
     {
+        // Je récupère le produit à ajouter au panier
+        $product = $produitRepo->find($id);
+        $entreprise_id = $product->getEntreprise()->getId();
+
+        // Récupérez ou créez un panier pour l'utilisateur connecté
+        $user = $this->getUser();
+        $entreprise = $user->getEntreprise();
+
+        $line = new LigneDevis();
+        $line->setProduit($product);
+        $line->setUsers($user);
+        $line->setEtat(0);
+        $line->setEntrepriseId($entreprise_id); //Produit->entreprise->id
+        // Vérifie si une ligne de devis existe déjà pour le user en cours avec un même produit->entreprise->id
+        $existingLine = $ligneRepo->findOneBy(['users' => $user, 'entrepriseId' => $entreprise_id]);
+
+        // Si une ligne existe déjà, récupérez l'id de devis de cette ligne
+        if ($existingLine) {
+            $devis = $existingLine->getDevis();
+        } else {
+            // Sinon, créez un nouveau devis pour l'entreprise en cours
+            $devis = new Devis();
+            $devis->setEntreprise($entreprise);
+            $devisRepo->save($devis, true);
+        }
+
+        //je lie ligne devis à devis
+        $line->setDevis($devis);
+        $ligneRepo->save($line, true);
+        return $this->redirectToRoute('app_devis');
+    }
+
+    #[Route('/devis/add/{id}', name: 'save_devis1')]
+    public function saveDevis(HttpFoundationRequest $request)
+    {
+        if ($request->isMethod('POST')) {
+            $name = $request->request->get('name');
+            // Traitement des données
+            // ...
+        }
         // Je récupère le produit à ajouter au panier
         $product = $produitRepo->find($id);
         $entreprise_id = $product->getEntreprise()->getId();
